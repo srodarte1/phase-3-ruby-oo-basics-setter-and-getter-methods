8b6d58ccaba80e03f5fcc028fcc144d1afad46bb
+ <rodarte_sarah@yahoo.com>
2023-01-10 22:37:48 -0700

    commit tree 1e7825b10d8e6cc1143c6124272f15a8dffca742

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Dog
+
+    def breed=(value)
+        @breed = value
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class Person
+    def job=(value)
+        @job = value
+    end
+    def job
+        @job
+    end
+
+    def name=(value)
+        @name = value
+    end
+    def name
+        @name
+    end
+
 
 end
